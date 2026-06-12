5d96035cad5abd53bc8c1632a3c95881e67dbea6
Meisam Alifallhi <alifallahimeisam@gmail.com>
2026-06-12 09:01:23 +0200

    Extract runtime inspection node modules

--- src/FluxMq.App/RuntimeNodeFactoryRegistryExtensions.cs
+++ src/FluxMq.App/RuntimeNodeFactoryRegistryExtensions.cs
@@ -2,11 +2,7 @@ using FluxMq.Core.Ids;
 using FluxMq.Core.Models;
 using FluxMq.Core.Mqtt;
 using FluxMq.Core.Secrets;
-using FluxMq.Components.JsonSchema;
-using FluxMq.Components.Logging;
 using FluxMq.Components.Mapping;
-using FluxMq.Components.MqttMetrics;
-using FluxMq.Components.MqttPayloadInspector;
 using FluxMq.Components.Storage.Repositories;
 using FluxMq.App.Metrics;
 using FluxFlow.Components.Http;
@@ -23,33 +19,14 @@ using FluxFlow.Components.Storage.FileSystem;
 using FluxFlow.Components.Storage.Options;
 using FluxFlow.Components.Timers;
 using FluxFlow.Components.Timers.Options;
-using FluxFlow.Engine.Components;
-using FluxFlow.Engine.Definitions;
 using FluxFlow.Engine.Mapping;
 using FluxFlow.Engine.Runtime;
-using FluxMq.Core.Metrics;
 using Microsoft.Extensions.DependencyInjection;
-using System.Text.Json;
-using static FluxMq.App.FluxMqRuntimeNodeConfigurationReader;
 
 namespace FluxMq.App;
 
 public static class RuntimeNodeFactoryRegistryExtensions
 {
-    private static readonly PortName InputPort = new("Input");
-    private static readonly PortName OutputPort = new("Output");
-    private static readonly PortName ResultPort = new("Result");
-    private static readonly PortName PassedPort = new("Passed");
-    private static readonly PortName FailedPort = new("Failed");
-    private static readonly PortName WhenTruePort = new("WhenTrue");
-    private static readonly PortName WhenFalsePort = new("WhenFalse");
-    private static readonly PortName ValidPort = new("Valid");
-    private static readonly PortName InvalidPort = new("Invalid");
-    private static readonly PortName SnapshotsPort = new("Snapshots");
-    private static readonly PortName EntriesPort = new("Entries");
-    private static readonly PortName FlowErrorsPort = new("FlowErrors");
-    private static readonly PortName ErrorsPort = new("Errors");
-
     public static RuntimeNodeFactoryRegistry RegisterPipelineComponentFactories(
         this RuntimeNodeFactoryRegistry registry,
         Func<MqttConnectionProfile, IMqttBrokerClient>? clientFactory = null,
@@ -139,139 +116,4 @@ public static class RuntimeNodeFactoryRegistryExtensions
             new MqttRecorderRuntimeNodeModule(),
             new FileWriterRuntimeNodeModule()
         ];
-
-    internal static RuntimeNode CreatePayloadInspector(NodeAddress address, NodeDefinition definition)
-    {
-        var component = new PayloadInspectorMapperComponent(boundedCapacity: GetBoundedCapacity(definition));
-
-        return RuntimeNode.Create(
-            address,
-            component,
-            inputs:
-            [
-                new InputPort<MqttEnvelope>(address.Port(InputPort), component.Input)
-            ],
-            outputs:
-            [
-                new OutputPort<InspectedMqttMessage>(address.Port(OutputPort), component.Output),
-                new OutputPort<FlowError>(address.Port(ErrorsPort), component.Errors)
-            ]);
-    }
-
-    internal static RuntimeNode CreateMqttMetrics(NodeAddress address, NodeDefinition definition)
-    {
-        var component = new MqttMetricsComponent(
-            boundedCapacity: GetBoundedCapacity(definition),
-            rateWindow: TimeSpan.FromSeconds(GetDoubleOrDefault(
-                definition,
-                "rateWindowSeconds",
-                MqttMetricsComponent.DefaultRateWindow.TotalSeconds)));
-
-        return RuntimeNode.Create(
-            address,
-            component,
-            inputs:
-            [
-                new InputPort<MqttEnvelope>(address.Port(InputPort), component.Input)
-            ],
-            outputs:
-            [
-                new OutputPort<MqttMetricsSnapshot>(address.Port(SnapshotsPort), component.Snapshots),
-                new OutputPort<FlowError>(address.Port(ErrorsPort), component.Errors)
-            ]);
-    }
-
-    internal static RuntimeNode CreateFlowLogger(NodeAddress address, NodeDefinition definition)
-    {
-        var hasMessageInput = definition.Ports.ContainsKey(InputPort.Value);
-        var hasFlowErrorInput = definition.Ports.ContainsKey(FlowErrorsPort.Value);
-        if (!hasMessageInput && !hasFlowErrorInput)
-        {
-            hasMessageInput = true;
-        }
-
-        var component = new FlowLoggerComponent(
-            boundedCapacity: GetBoundedCapacity(definition),
-            maxEntries: GetIntOrDefault(definition, "maxEntries", 500, minValue: 1),
-            includePayloadPreview: GetBoolOrDefault(definition, "includePayloadPreview", false),
-            maxPayloadPreviewChars: GetIntOrDefault(definition, "maxPayloadPreviewChars", 512, minValue: 1),
-            waitForMessageInputCompletion: hasMessageInput,
-            waitForFlowErrorInputCompletion: hasFlowErrorInput);
-
-        return RuntimeNode.Create(
-            address,
-            component,
-            inputs:
-            [
-                new InputPort<MqttEnvelope>(address.Port(InputPort), component.Input),
-                new InputPort<FlowError>(address.Port(FlowErrorsPort), component.FlowErrors)
-            ],
-            outputs:
-            [
-                new OutputPort<FlowLogEntry>(address.Port(EntriesPort), component.Entries),
-                new OutputPort<FlowError>(address.Port(ErrorsPort), component.Errors)
-            ]);
-    }
-
-    internal static RuntimeNode CreateJsonSchemaValidator(NodeAddress address, NodeDefinition definition)
-    {
-        var component = new JsonSchemaValidatorComponent(
-            GetJsonSchemaValidatorDefinition(definition),
-            boundedCapacity: GetBoundedCapacity(definition));
-
-        return RuntimeNode.Create(
-            address,
-            component,
-            inputs:
-            [
-                new InputPort<MqttEnvelope>(address.Port(InputPort), component.Input)
-            ],
-            outputs:
-            [
-                new OutputPort<JsonSchemaValidationResult>(address.Port(ResultPort), component.Result),
-                new OutputPort<MqttEnvelope>(address.Port(ValidPort), component.Valid),
-                new OutputPort<MqttEnvelope>(address.Port(InvalidPort), component.Invalid),
-                new OutputPort<FlowError>(address.Port(ErrorsPort), component.Errors)
-            ]);
-    }
-
-    private static JsonSchemaValidatorDefinition GetJsonSchemaValidatorDefinition(NodeDefinition definition)
-    {
-        var schema = GetOptionalJsonSchema(definition);
-        var schemaPath = GetNullableString(definition, "schemaPath") ?? GetNullableString(definition, "schemaFile");
-        if (schema is null && string.IsNullOrWhiteSpace(schemaPath))
-        {
-            throw new InvalidOperationException(
-                "JSON Schema validator requires configuration value 'schema' or 'schemaPath'.");
-        }
-
-        return new JsonSchemaValidatorDefinition
-        {
-            SchemaJson = schema,
-            SchemaPath = schemaPath,
-            SchemaId = GetNullableString(definition, "schemaId") ?? schemaPath ?? "inline"
-        };
-    }
-
-    private static string? GetOptionalJsonSchema(NodeDefinition definition)
-    {
-        if (!definition.Configuration.TryGetValue("schema", out var value))
-        {
-            return null;
-        }
-
-        if (value.ValueKind == JsonValueKind.Null)
-        {
-            return null;
-        }
-
-        if (value.ValueKind == JsonValueKind.String)
-        {
-            var schema = value.GetString();
-            return string.IsNullOrWhiteSpace(schema) ? null : schema;
-        }
-
-        return value.GetRawText();
-    }
-
 }
